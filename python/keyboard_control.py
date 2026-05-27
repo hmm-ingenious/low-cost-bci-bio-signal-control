d8fb6587a04c38bbcff77d2c7874cd34af77b1b5
hmm-ingenious <aryanpatgar19@gmail.com>
2026-05-27 14:32:52 +0530

    Update keyboard_control.py

--- python/keyboard_control.py
+++ python/keyboard_control.py
@@ -1,1 +1,24 @@
+import serial
+import keyboard
 
+ser = serial.Serial('COM3', 115200)
+
+print("BCI Control Started...")
+
+while True:
+    try:
+        line = ser.readline().decode().strip()
+
+        if line:
+            print("Detected:", line)
+
+            if line == "BLINK":
+                keyboard.press_and_release('space')
+                print("Jump")
+
+            elif line == "MUSCLE":
+                keyboard.press_and_release('down')
+                print("Crouch")
+
+    except Exception as e:
+        print("Error:", e)
